10233094f71de96d7922819caed3b166e4a84381
Joshua Hamlet <jhamlet@tuta.io>
2022-01-09 21:31:15 -0800

    Switch to js file to avoid Netlify error

--- pages/checkout/index.js
+++ pages/checkout/index.js
@@ -16,7 +16,7 @@ const Checkout = ({checkout}) => {
   )
 }
 
-export const getStaticProps: GetStaticProps = async() => {
+export const getStaticProps = async() => {
   
   const client = Client.buildClient({
     domain: process.env.REACT_APP_SHOPIFY_DOMAIN,
